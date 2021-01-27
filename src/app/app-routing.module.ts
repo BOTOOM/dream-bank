b5b89c7f21c385149450d3182d73d66650c697fb
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-01-27 12:48:53 -0500

    confgure main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
