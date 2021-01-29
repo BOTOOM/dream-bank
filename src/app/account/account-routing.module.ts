72061dc8b3a9b4f823196ffdc4804db2f5c05594
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-01-28 20:14:24 -0500

    basic table

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllAcountsComponent } from './components/all-acounts/all-acounts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AllAcountsComponent,
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatCardModule} from '@angular/material/card';
+
+
 import { AccountRoutingModule } from './account-routing.module';
 import { AllAcountsComponent } from './components/all-acounts/all-acounts.component';
+import { StatusBarComponent } from './components/status-bar/status-bar.component';
 
 
 @NgModule({
-  declarations: [AllAcountsComponent],
+  declarations: [AllAcountsComponent, StatusBarComponent],
   imports: [
     CommonModule,
+    MatCardModule,
     AccountRoutingModule
   ]
 })
