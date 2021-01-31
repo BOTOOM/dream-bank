598a9b6fda6e61cb4fb176c5a9a9f8df4db5552a
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-01-30 23:58:02 -0500

    basic add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers, metaReducers } from './reducers';
 
 
 @NgModule({
@@ -17,7 +21,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    !environment.production ? StoreDevtoolsModule.instrument({ }) : [],
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: false,
+        strictActionImmutability: false,
+      }
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    // StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
 
   ],
   providers: [],
